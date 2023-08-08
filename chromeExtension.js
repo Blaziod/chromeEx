9fa28821418bae23b8b70887863b2a509dbb04cc
unknown <netstantechnology@gmail.com>
2023-08-08 21:40:11 +0100

    my chrome extension 1.0

--- chromeExtension.js
+++ chromeExtension.js
@@ -44,6 +44,4 @@ inputBtn.addEventListener("click", function () {
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
   render(myLeads);
-
-  console.log(localStorage.getItem("myLeads"));
 });
